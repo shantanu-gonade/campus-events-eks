f9a4b6402f0b2c2575fba6fe09e52f55d72db81a
simran-mohapatra <simran.mohapatra.20@gmail.com>
2025-12-05 22:39:19 -0500

    karpenter autoscaling config

---
+++ terraform/modules/karpenter/outputs.tf
@@ -0,0 +1,39 @@
+output "controller_role_arn" {
+  description = "ARN of the Karpenter controller IAM role"
+  value       = aws_iam_role.karpenter_controller.arn
+}
+
+output "controller_role_name" {
+  description = "Name of the Karpenter controller IAM role"
+  value       = aws_iam_role.karpenter_controller.name
+}
+
+output "node_role_arn" {
+  description = "ARN of the Karpenter node IAM role"
+  value       = aws_iam_role.karpenter_node.arn
+}
+
+output "node_role_name" {
+  description = "Name of the Karpenter node IAM role"
+  value       = aws_iam_role.karpenter_node.name
+}
+
+output "node_instance_profile_name" {
+  description = "Name of the Karpenter node instance profile"
+  value       = aws_iam_instance_profile.karpenter_node.name
+}
+
+output "node_instance_profile_arn" {
+  description = "ARN of the Karpenter node instance profile"
+  value       = aws_iam_instance_profile.karpenter_node.arn
+}
+
+output "sqs_queue_name" {
+  description = "Name of the SQS queue for Karpenter interruption handling"
+  value       = aws_sqs_queue.karpenter.name
+}
+
+output "sqs_queue_url" {
+  description = "URL of the SQS queue for Karpenter interruption handling"
+  value       = aws_sqs_queue.karpenter.url
+}
